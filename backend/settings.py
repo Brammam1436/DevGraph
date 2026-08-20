52db08588928de10cad330e4ede007838af52097
Brammam <brammambalam@gmail.com>
2026-08-20 20:34:00 +0530

    Prepare application for production

--- backend/settings.py
+++ backend/settings.py
@@ -24,18 +24,17 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".onrender.com",
+    "devgraph-1.onrender.com",
 ]
 
 if os.getenv("RENDER_EXTERNAL_HOSTNAME"):
     ALLOWED_HOSTS.append(os.getenv("RENDER_EXTERNAL_HOSTNAME"))
-
 
 # Application definition
 
